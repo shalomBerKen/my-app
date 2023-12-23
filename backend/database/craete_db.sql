91b9c3e0322621d0d5327786a659af0d0060702b
shalomBerKen <126160877+shalomBerKen@users.noreply.github.com>
2023-12-23 22:06:45 +0200

    changes in db primeries and data

--- backend/database/craete_db.sql
+++ backend/database/craete_db.sql
@@ -1,10 +1,10 @@
-CREATE TABLE `tasks` (
+CREATE TABLE `tasks`(
     `id_task` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `task_name` VARCHAR(255) NOT NULL,
     `task_details` VARCHAR(255) NOT NULL,
     `task_date` DATE NOT NULL,
-    `is_done` TINYINT NOT NULL,
-    PRIMARY KEY `tasks_id_task_primary` (`id_task`)
+    `is_done` TINYINT(1) NOT NULL,
+    PRIMARY KEY `tasks_id_task_primary`(`id_task`)
 );
 
 CREATE TABLE `users`(
@@ -14,17 +14,17 @@ CREATE TABLE `users`(
     PRIMARY KEY `users_id_user_primary`(`id_user`)
 );
 
+CREATE TABLE `communities_tasks`(
+    `id_community` BIGINT NOT NULL,
+    `id_task` BIGINT NOT NULL,
+    PRIMARY KEY `communities_tasks_id_primary`(`id_community`, `id_task`)
+);
+
 CREATE TABLE `task_users`(
     `user_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `task_id` BIGINT NOT NULL,
     `received_approv` BIGINT NOT NULL,
     PRIMARY KEY `task_users_user_id_primary`(`user_id`, `task_id`)
-);
-
-CREATE TABLE `communities_tasks`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `id_community` BIGINT NOT NULL,
-    `id_task` BIGINT NOT NULL
 );
 
 CREATE TABLE `communities`(
@@ -35,21 +35,8 @@ CREATE TABLE `communities`(
 );
 
 CREATE TABLE `users_communities`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `id_user` BIGINT NOT NULL,
     `id_community` BIGINT NOT NULL,
     `is_manager` BIGINT NOT NULL,
-    PRIMARY KEY `users_communities_id_primary`(`id`)
+    PRIMARY KEY `users_communities_id_primary`(`id_user`, `id_community`)
 );
-ALTER TABLE
-    `users_communities` ADD PRIMARY KEY `users_communities_id_primary`(`id`);
-ALTER TABLE
-    `task_users` ADD CONSTRAINT `task_users_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id_user`);
-ALTER TABLE
-    `users_communities` ADD CONSTRAINT `users_communities_id_community_foreign` FOREIGN KEY(`id_community`) REFERENCES `communities`(`id_community`);
-ALTER TABLE
-    `users_communities` ADD CONSTRAINT `users_communities_id_user_foreign` FOREIGN KEY(`id_user`) REFERENCES `users`(`id_user`);
-ALTER TABLE
-    `communities` ADD CONSTRAINT `communities_id_community_foreign` FOREIGN KEY(`id_community`) REFERENCES `tasks`(`id_task`);
-ALTER TABLE
-    `tasks` ADD CONSTRAINT `tasks_id_task_foreign` FOREIGN KEY(`id_task`) REFERENCES `task_users`(`task_id`);
--- backend/database/test_data.sql
+++ backend/database/test_data.sql
@@ -1,40 +1,77 @@
-INSERT INTO tasks (task_name, task_details, task_date, is_done) VALUES
-('Task 1', 'Details for Task 1', '2023-01-01', 0),
-('Task 2', 'Details for Task 2', '2023-02-01', 1),
-('Task 3', 'Details for Task 3', '2023-03-01', 0),
-('Task 4', 'Details for Task 4', '2023-04-01', 1),
-('Task 5', 'Details for Task 5', '2023-05-01', 0);
--- Add more rows as needed
-INSERT INTO users (user_name, user_password) VALUES
-('user1', 'password1'),
-('user2', 'password2'),
-('user3', 'password3'),
-('user4', 'password4'),
-('user5', 'password5');
--- Add more rows as needed
-INSERT INTO task_users (user_id, task_id, received_approv) VALUES
-(1, 1, 1),
-(2, 1, 0),
-(3, 2, 1),
-(4, 2, 0),
-(5, 3, 1);
--- Add more rows as needed
-INSERT INTO communities (community_name, community_details) VALUES
-('Community 1', 'Details for Community 1'),
-('Community 2', 'Details for Community 2'),
-('Community 3', 'Details for Community 3'),
-('Community 4', 'Details for Community 4'),
-('Community 5', 'Details for Community 5');
--- Add more rows as needed
-INSERT INTO users_communities (id_user, id_community, is_manager) VALUES
-(1, 1, 1),
-(2, 1, 0),
-(3, 2, 1),
-(4, 2, 0),
-(5, 3, 1),
-(1, 4, 1),
-(2, 4, 0),
-(3, 5, 1),
-(4, 5, 0),
-(5, 5, 1);
--- Add more rows as needed
+-- Insert data into 'users' table
+INSERT INTO `users` (`user_name`, `user_password`) VALUES
+    ('JohnDoe', 'password123'),
+    ('JaneSmith', 'letmein'),
+    ('BobJohnson', 'securepass'),
+    ('AliceWhite', '123456'),
+    ('EvaGreen', 'p@ssw0rd'),
+    ('CharlieBrown', 'qwerty'),
+    ('DavidLee', 'pass123'),
+    ('GraceTaylor', 'password!'),
+    ('SamuelBlack', '987654'),
+    ('OliviaDavis', 'ilovecoding');
+
+-- Insert data into 'communities' table
+INSERT INTO `communities` (`community_name`, `community_details`) VALUES
+    ('Tech Enthusiasts', 'A community for tech lovers'),
+    ('Fitness Freaks', 'Health and fitness enthusiasts'),
+    ('Book Club', 'For avid readers'),
+    ('Travelers', 'Exploring the world together'),
+    ('Foodies', 'For those who love food'),
+    ('Photography Club', 'Capturing moments through lenses'),
+    ('Music Lovers', 'For music enthusiasts'),
+    ('Artists Hub', 'Connecting artists worldwide'),
+    ('Gaming Community', 'For gamers by gamers'),
+    ('Coding Wizards', 'The coding community');
+
+-- Insert data into 'tasks' table
+INSERT INTO `tasks` (`task_name`, `task_details`, `task_date`, `is_done`) VALUES
+    ('Complete Project X', 'Finish all tasks related to Project X', '2023-01-10', 0),
+    ('Gym Workout', 'Cardio and strength training', '2023-01-15', 1),
+    ('Discuss Book of the Month', 'Share thoughts on this month''s book', '2023-01-20', 0),
+    ('Plan Trip to Europe', 'Research and plan the itinerary', '2023-02-01', 0),
+    ('Explore Local Cuisine', 'Visit new restaurants in the city', '2023-02-10', 1),
+    ('Photo Exhibition Preparation', 'Select and print photos for the exhibition', '2023-02-15', 0),
+    ('Music Jam Session', 'Collaborative music creation', '2023-03-01', 0),
+    ('Art Collaboration Project', 'Work on a joint art project', '2023-03-10', 1),
+    ('Gaming Tournament', 'Organize and participate in a gaming tournament', '2023-03-15', 0),
+    ('Code Review Session', 'Review and improve code together', '2023-03-20', 1);
+
+-- Insert data into 'communities_tasks' table
+INSERT INTO `communities_tasks` (`id_community`, `id_task`) VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+    (6, 6),
+    (7, 7),
+    (8, 8),
+    (9, 9),
+    (10, 10);
+
+-- Insert data into 'users_communities' table
+INSERT INTO `users_communities` (`id_user`, `id_community`, `is_manager`) VALUES
+    (1, 1, 1),
+    (2, 2, 1),
+    (3, 3, 0),
+    (4, 4, 1),
+    (5, 5, 0),
+    (6, 6, 1),
+    (7, 7, 0),
+    (8, 8, 1),
+    (9, 9, 0),
+    (10, 10, 1);
+
+-- Insert data into 'task_users' table
+INSERT INTO `task_users` (`user_id`, `task_id`, `received_approv`) VALUES
+    (1, 1, 0),
+    (2, 2, 1),
+    (3, 3, 0),
+    (4, 4, 1),
+    (5, 5, 0),
+    (6, 6, 1),
+    (7, 7, 0),
+    (8, 8, 1),
+    (9, 9, 0),
+    (10, 10, 1);
