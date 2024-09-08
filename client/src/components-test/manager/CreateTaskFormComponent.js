1bef2f9a0903a08d90894db5f70e1c9cf151e52f
shalomBerKen <126160877+shalomBerKen@users.noreply.github.com>
2024-09-08 17:30:42 +0300

    Update CreateTaskFormComponent.js
    putting the map on the right side of the page

--- client/src/components-test/manager/CreateTaskFormComponent.js
+++ client/src/components-test/manager/CreateTaskFormComponent.js
@@ -8,12 +8,12 @@ import {
   FormHelperText,
   Button,
   Box,
+  Flex,  // נוסיף Flex מ-Chakra UI
 } from '@chakra-ui/react';
 
 import MapContainer from './map/GoogleMap';
 
 const CreateTaskFormComponent = () => {
-
   const navigate = useNavigate();
   const { id } = useParams();
   const [formData, setFormData] = useState({
@@ -22,10 +22,10 @@ const CreateTaskFormComponent = () => {
     task_details: '',
     task_date: '',
     is_done: false,
-    address: '',  // הפיכת כתובת לחובה
+    address: '', 
   });
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     let value = e.target.value;
     setFormData({
       ...formData,
@@ -33,12 +33,22 @@ const CreateTaskFormComponent = () => {
     });
   };
 
-  const handleSubmit = async e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
       const currentDate = new Date();
-      const formattedDateTime = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')} ${currentDate.getHours().toString().padStart(2, '0')}:${currentDate.getMinutes().toString().padStart(2, '0')}:${currentDate.getSeconds().toString().padStart(2, '0')}`;
+      const formattedDateTime = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1)
+        .toString()
+        .padStart(2, '0')}-${currentDate.getDate()
+        .toString()
+        .padStart(2, '0')} ${currentDate.getHours()
+        .toString()
+        .padStart(2, '0')}:${currentDate.getMinutes()
+        .toString()
+        .padStart(2, '0')}:${currentDate.getSeconds()
+        .toString()
+        .padStart(2, '0')}`;
 
       const response = await axiosInstance.post('/tasks/', {
         ...formData,
@@ -52,54 +62,55 @@ const CreateTaskFormComponent = () => {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <FormControl w={'xl'} m={'auto'} p={24} isRequired>
-          <FormLabel>Name</FormLabel>
-          <Input
-            type="text"
-            name="task_name"
-            value={formData.task_name}
-            onChange={handleChange}
-            required
-          />
-          <FormHelperText>
-            It is recommended to choose a short and catchy name.
-          </FormHelperText>
-          <br />
-          <FormLabel>Description</FormLabel>
-          <Input
-            type="text"
-            name="task_details"
-            value={formData.task_details}
-            onChange={handleChange}
-            required
-          />
-          <FormHelperText>
-            Describe the nature of your task and the goals you promote.
-          </FormHelperText>
-          <br />
-          <FormLabel>Address</FormLabel>
-          <Input
-            type="text"
-            name="address"
-            value={formData.address}
-            onChange={handleChange}
-            required
-          />
-          <FormHelperText>
-            Please provide the task location.
-          </FormHelperText>
-          <Button colorScheme="blue" mt={12} type="submit">
-            Create
-          </Button>
-        </FormControl>
-      </form>
-
-      <Box width="600px" margin="auto" marginBottom="16px">
+    <Flex direction={{ base: 'column', md: 'row' }} w="full" p={6} alignItems="flex-start">
+      <Box flex="1" p={4}>
+        <form onSubmit={handleSubmit}>
+          <FormControl isRequired>
+            <FormLabel>Task Name</FormLabel>
+            <Input
+              type="text"
+              name="task_name"
+              value={formData.task_name}
+              onChange={handleChange}
+              required
+            />
+            <FormHelperText>
+              It is recommended to choose a short and catchy name.
+            </FormHelperText>
+            <br />
+            <FormLabel>Description</FormLabel>
+            <Input
+              type="text"
+              name="task_details"
+              value={formData.task_details}
+              onChange={handleChange}
+              required
+            />
+            <FormHelperText>
+              Describe the nature of your task and the goals you promote.
+            </FormHelperText>
+            <br />
+            <FormLabel>Address</FormLabel>
+            <Input
+              type="text"
+              name="address"
+              value={formData.address}
+              onChange={handleChange}
+              required
+            />
+            <FormHelperText>
+              Please provide the task location.
+            </FormHelperText>
+            <Button colorScheme="blue" mt={12} type="submit">
+              Create
+            </Button>
+          </FormControl>
+        </form>
+      </Box>
+      <Box flex="1" p={4} maxWidth="600px">
         <MapContainer formData={formData} />
       </Box>
-    </>
+    </Flex>
   );
 };
 
